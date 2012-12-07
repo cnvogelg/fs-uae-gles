3d87ea3bef1dd00b780c8ccd06c8d9a120d2048b
Christian Vogelgsang <C.Vogelgsang@web.de>
2012-12-07 15:07:48 +0100

    small fixes

--- pandora/run.sh
+++ pandora/run.sh
@@ -3,5 +3,16 @@
 # run.sh - pandora runner for FS-UAE
 #
 
+# ensure configuration directory is here
+if [ ! -d Configurations ]; then
+    mkdir Configurations
+fi
+
+# copy default config if none is available
+if [ ! -e Configurations/Default.fs-uae ]; then
+    cp Default.fs-uae.templ Configurations/Default.fs-uae
+fi
+
+# run fs-uae
 export LD_LIBRARY_PATH=.
-./fs-uae --base-dir=$(HOME)
+./fs-uae --base-dir=$HOME
