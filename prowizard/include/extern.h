bc838824fa3b22e56b0265af2698ff7822dab897
Christian Vogelgsang <C.Vogelgsang@web.de>
2014-10-29 21:26:53 +0100

    osx prowiz fix

--- prowizard/include/extern.h
+++ prowizard/include/extern.h
@@ -331,7 +331,9 @@ extern short test_smps ( long, long, long, Uchar, Uchar );
 extern long  PWGetFileSize ( char * );
 extern FILE * PW_fopen ( char *, char * );
 extern void fillPTKtable ( Uchar[37][2] );
+#ifndef __APPLE__
 extern int htonl(int);
+#endif
 
 /* globals */
 /* Some say it's badly coding when using Globals ... sure it is, now what's the solution ? */
--- prowizard/misc/misc.c
+++ prowizard/misc/misc.c
@@ -509,8 +509,10 @@ void fillPTKtable (Uchar poss[37][2])
 }
 
 #ifndef _WIN64
+#ifndef __APPLE__
 int htonl(int v)
 {
 	return ((v >> 24) & 0x000000ff) | ((v >> 8) & 0x0000ff00) | ((v << 8) & 0x00ff0000) | ((v << 24) & 0xff000000);
 }
+#endif
 #endif
